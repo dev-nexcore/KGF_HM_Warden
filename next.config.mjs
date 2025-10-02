941de9eb08a402ac16c754b39f69e39efad1097f
Akshi1277 <verstapenalfred@gmail.com>
2025-10-02 10:16:23 +0530

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/warden',
+    assetPrefix: '/warden/',
+    trailingSlash: true,
+};
 
 export default nextConfig;
